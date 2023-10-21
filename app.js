4dcbe6226a0b361cb984677646da7682b97a9e5f
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2023-10-21 04:07:07 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,329 @@
+// _________________ MATH EXPRESSIONS Ch#5____________________ 
+
+// ------------- Question No.1-------------------
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 + num2;
+// document.write("Sum of 3 and 5 is" + " " + total);
+
+// ------------- Question No.2-------------------
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 - num2;
+// document.write("Sum of 3 and 5 is" + " " + total);
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 * num2;
+// document.write("Sum of 3 and 5 is" + " " + total);
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 / num2;
+// document.write("Sum of 3 and 5 is" + " " + total);
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 % num2;
+// document.write("Sum of 3 and 5 is" + " " + total);
+
+
+// ------------- Question No.3-------------------
+
+// var num;
+// document.write("Value after variable declaration is:" + num + "<br>");
+// num = 5;
+// document.write("Initial value:" + num + " " + "<br>");
+// num++;
+// document.write("Value after increment is:" + num + "  " +  "<br>");
+//  var num1 = num + 7;
+//  document.write("Value after addition is:" + num1 + "  " +  "<br>");
+//  num1--;
+//  document.write("Value after decrement is:" + num1 + "  " +  "<br>");
+//   var remainder = num1 % 3;
+//   document.write("The remainder is :" + remainder + "  ");
+
+  // ------------- Question No.4-------------------
+
+//   var ticket = 600;
+//   var buying = 5;
+//   var totalCost = ticket * buying;
+//   document.write("Total cost to buy ticket to movie is" + " "  + totalCost + "PKR"); 
+
+  // ------------- Question No.5-------------------
+
+  // document.write(" <h3>Table Of 4<h3/> ")
+  // var num = 4;
+  // document.write("4 X 1 =" + num*1 + "<br>" + "4 X 2 =" + num*2 + "<br>"
+  // + "4 X 3 =" + num*3 + "<br>" + "4 X 4 =" + num*4 + "<br>" + "4 X 5 =" + num*5 + "<br>"
+  // + "4 X 6 =" + num*6 + "<br>" + "4 X 7 =" + num*7 + "<br>" + "4 X 8 =" + num*8 + "<br>" 
+  // + "4 X 9 =" + num*9 + "<br>" + "4 X 10 =" + num*10 + "<br>")
+
+   // ------------- Question No.6-------------------
+
+  // var celsiusTemperature = 25;
+  // var convertFahrenheit = (celsiusTemperature * 9 / 5)+32; 
+  // document.write("25oC is" + " "  + convertFahrenheit + "oF" + "<br>");
+  
+
+  // var FahrenheitTemperature = 70;
+  // var convertCelsius = (FahrenheitTemperature - 32) * 5/9; 
+  // document.write("75oF is"  + " " + convertCelsius+  "oC");
+
+    // ------------- Question No.7-------------------
+// document.write( " Shopping Cart " + "<br>" );
+// var price1 = 650;
+// document.write("Price of item 1 is" + " " + price1 + "<br>" );
+// var quantity1 = 3;
+// document.write("Ordered quantity of item 1 is"  + " " + quantity1 + "<br>");
+// var price2 = 100;
+// document.write("Price of item 2 is"  + " " + price2 + "<br>");
+// var quantity2 = 7;
+// document.write("Ordered quantity of item 2 is"  + " " + quantity2 + "<br>");
+// var shippingCharges = 100;
+// document.write("Shipping Charges" + " " + shippingCharges + "<br>");
+// var totalCost = ( price1*quantity1 ) +  ( price2*quantity2 ) + shippingCharges;
+// document.write("Total cost of your order is"  + " " + " " + totalCost); 
+
+  // ------------- Question No.8-------------------
+
+  // var totalMarks = 980;
+  // document.write("Total marks:" + totalMarks +"<br>");
+  // var marksObtained = 804;
+  // document.write("Marks obtained:" + marksObtained + "<br>");
+  // var percentage = (marksObtained * 100) / totalMarks;
+  // document.write("Percentage:" + percentage + "%"); 
+
+
+    // ------------- Question No.9-------------------
+
+    // var USDollars = 10*104.80;
+    // var SaudiRiyals = 25*28;
+    // var PakistaniRupee = USDollars+SaudiRiyals;
+
+    // document.write("Total Currency in PKR :" + " "+ PakistaniRupee);
+
+    
+        // ------------- Question No.10-------------------
+ 
+// var num = 100;
+// var result = ((num+5)*10)/2;
+// document.write(result);         
+        
+
+        // ------------- Question No.11-------------------
+
+// var CurrentYear = 2016;
+// var brithYear = 1992;
+// var Age = CurrentYear - brithYear;
+// document.write("Your Age is :" + " " + Age);
+
+    // ------------- Question No.12-------------------
+
+// document.write("The Geometrizer");
+// var radius = 20;
+// document.write("Radius of a circle :" + " " + radius + "<br>");
+// var circumference = 2*3.142*20;
+// document.write("The circumference is :" + " " +  circumference + "<br>");
+// var area = 3.142 * radius*radius;
+// document.write("The area id:" + " " + area);   
+
+        // ------------- Question No.13-------------------
+        
+        // document.write("<h1>The Lifetime Supply Calculator<h1/>");
+        // var favoriteSnack = "chocolate chip";
+        // var CurrentAge = 15;
+        // var EstimatedMaximumAge = 65;
+        // var AmountPerDay = 3;
+        // var Age = EstimatedMaximumAge - CurrentAge;
+        // var EatTotal = Age * AmountPerDay;
+        // document.write("favorite Snack :" + " " + favoriteSnack + "<br>");
+        // document.write("Current Age :" + " " + CurrentAge + "<br>");
+        // document.write("Estimated Maximum Age:" + " " + EstimatedMaximumAge + "<br>");
+        // document.write("Amount of snacks per day:" + " " + AmountPerDay + "<br>");
+        // document.write("You will need"+" "+ EatTotal+" "+  favoriteSnack +" " +  "to last you until the ripe old age of" + " " + EstimatedMaximumAge); 
+
+
+      
+// _________________ MATH EXPRESSIONS CH#6 TO 9____________________
+
+     // ------------- Question No.1-------------------
+
+//  document.write("Result:" + "<br>");    
+//  var a = 10;
+//  document.write("The value of a is:" + " " + a + "<br>");
+//  document.write("---------------------------------" + "<br>");    
+//  ++a;
+//  document.write("The value of ++a is:" + a + "<br>");     
+//  document.write("Now the value of a is:" + a + "<br>"); 
+//  a++;
+//  document.write("The value of a++= is:" + a + "<br>");     
+//  document.write("Now the value of a is:" + a + "<br>"); 
+//  --a;
+//  document.write("The value of --a is:" + a + "<br>");     
+//  document.write("Now the value of a is:" + a + "<br>"); 
+//  a--;
+//  document.write("The value of a-- is:" + a + "<br>");     
+//  document.write("Now the value of a is:" + a + "<br>"); 
+
+  // ------------- Question No.2-------------------
+
+  // var a = 2;
+  // var b =  1;
+  // var result = --a - --b + ++b + b--;
+        // Explain the output at each stage:
+     // a is 2 and b is 1 
+   // --a; a is 2 --2 decrement value now a is 1
+   // --a - --b;  a is 2 and b is 1  decrement value now a is 1 and b is 0 result is 1-0=1   
+   // --a - --b + ++b; a is 1 and b is 0  decrement value now a is 0 and b is 0 and increment b now b is 1 result is 0-0+1=1
+  // --a - --b + ++b + b--; a is 0 and b is 1 decrement value now a is -1 and b is 0 and increment b now b is 1 result is -1-0+1+1=1
+  // document.write(result);
+
+  
+   // ------------- Question No.3-------------------
+
+  //  var user = prompt("Enter your name");
+  //  document.write("Hello" +" "+user +" "+ "! Welcome To our website.")
+
+   // ------------- Question No.4-------------------
+
+  //  var num = +prompt("Enter Number");
+  //  if(num){
+  //   document.write(num + " " +"X 1 =" + num*1 + "<br>" + num + " " +"X 2 =" + num*2 + "<br>"
+  // + num + " " + "X 3 =" + num*3 + "<br>" + num + " " +"X 4 =" + num*4 + "<br>" + num + " " +"X 5 =" + num*5 + "<br>"
+  // + "4 X 6 =" + num*6 + "<br>" + "4 X 7 =" + num*7 + "<br>" + "4 X 8 =" + num*8 + "<br>" 
+  // + num + " " +" X 9 =" + num*9 + "<br>" + num + " " +" X 10 =" + num*10 + "<br>")
+  //  }else{
+  //    document.write("5 X 1 =" + 5*1 + "<br>" + "5 X 2 =" + 5*2 + "<br>"
+  // + "5 X 3 =" + 5*3 + "<br>" + "5 X 4 =" + 5*4 + "<br>" + "5 X 5 =" + 5*5 + "<br>"
+  // + "5 X 6 =" + 5*6 + "<br>" + "5 X 7 =" + 5*7 + "<br>" + "5 X 8 =" + 5*8 + "<br>" 
+  // + "5 X 9 =" + 5*9 + "<br>" + "5 X 10 =" + 5*10 + "<br>")
+  //  }
+
+// ------------- Question No.5-------------------
+
+// var subject1 = prompt("Enter First Subject");
+// var subject2 = prompt("Enter Second Subject");
+// var subject3 = prompt("Enter Third Subject");
+
+// var subject1mark = +prompt("Enter First Subject Marks"); 
+// var subject2mark = +prompt("Enter Second Subject Marks"); 
+// var subject3mark = +prompt("Enter Third Subject Marks"); 
+// var TotalObtainedM = subject3mark+subject3mark+subject3mark;
+// var TotalMarks = 100;
+// var subject1Percentage = (subject1mark*100)/100;
+// var subject2Percentage = (subject2mark*100)/100;
+// var subject3Percentage = (subject3mark*100)/100;
+// var TotalMarksAllSubject = 300;
+
+// var TotalPercentage = ((subject1Percentage+subject2Percentage+subject3Percentage)*100)/300;
+// document.write("Subject" + "  " + "Total Marks" + "  " + "Obtaine Marks" +"  " + "Percentage" + "<br>");
+// document.write(subject1 + "         " + TotalMarks + "        " +subject1mark + "         " + subject1Percentage + " %"+ "<br>");
+// document.write(subject2 + "         " + TotalMarks + "        " +subject2mark + "         " + subject2Percentage + " %"+ "<br>");
+// document.write(subject3 + "         " + TotalMarks + "        " +subject3mark + "         " + subject3Percentage + " %"+ "<br>");
+// document.write(" "+ " "+ " "+" "+ " "+ " "+" "+ " "+ " " +TotalMarksAllSubject + " "+ " "+ TotalObtainedM + + " "+ " " + TotalPercentage);
+
+// ______________Uer Input and Coditional Statment CH#9 TO 11____________________
+
+// ------------- Question No.1-------------------
+// var city = prompt("Enter City");
+// if(city == "Karachi"){
+// document.write("Welcome to city of light");
+// }else{
+//   document.write("Welcome"); 
+// }
+// ------------- Question No.2-------------------
+
+// var gender = prompt("Enter Your Gender");
+// if(gender == "Male"){
+//   document.write("Good Morning Sir");
+// }
+// else if(gender == "Female"){
+// document.write("Good Morning Ma’am");
+// }
+
+  // ------------- Question No.3-------------------
+
+//   var color = prompt("Enter color of road traffic signal");
+//   if(color == "Red"){
+// document.write("Must Stop");
+// }
+// else if(color == "Yellow")
+// {
+//   document.write("Ready to move");
+
+//   }
+
+//   else if(color == "Green")
+// {
+//   document.write("Move now");
+  
+//   }
+//   else{
+//     document.write("Enter Color");
+//   }
+
+// ------------- Question No.4-------------------
+// var fuel =prompt("Enter fue in car in litres");
+// if(fuel < 0.25){
+// document.write("Please refill the fuel in your car");
+// }else{
+//   document.write("fuel is good")
+// }
+
+// ------------- Question No.5-------------------
+
+// ------------- Question No.6-------------------
+
+// var subjectMark1 = +prompt("Enter First Subject Marks");
+// var subjectMark2 = +prompt("Enter Second Subject Marks");
+// var subjectMark3 = +prompt("Enter Third Subject Marks");
+// var total = subjectMark1+subjectMark1+subjectMark1;
+// var percentage = (total*100)/300;
+
+// if (  percentage >= 80){
+// document.write( "Total marks :" + " " + "300"  + "<br>"
+// +"Marks obtained :" + total+ "<br>" + "Percentage :" + " "+  percentage +  "<br>"
+// +"Grade : " + " " + "A-one"+ "<br>" + "Remarks :" +" " + " Excellent"
+// )
+// }
+// else if (  percentage >= 70){
+//   document.write( "Total marks :" + " " + "300"  + "<br>"
+//   +"Marks obtained :" + total+ "<br>" + "Percentage :" + " "+  percentage +  "<br>"
+//   +"Grade : " + " " + "A"+ "<br>" + "Remarks :" +" " + "Good"
+//   )
+//   }
+//   else if (  percentage >= 60){
+//     document.write( "Total marks :" + " " + "300"  + "<br>"
+//     +"Marks obtained :" + total+ "<br>" + "Percentage :" + " "+  percentage +  "<br>"
+//     +"Grade : " + " " + "B"+ "<br>" + "Remarks :" +" " + "You need to improve"
+//     )
+//     }
+
+//     else if (  percentage < 60){
+//       document.write( "Total marks :" + " " + "300"  + "<br>"
+//       +"Marks obtained :" + total+ "<br>" + "Percentage :" + " "+  percentage +  "<br>"
+//       +"Grade : " + " " + "Fail"+ "<br>" + "Remarks :" +" " + "Sorry"
+//       )
+//       }
+      
+
+// ------------- Question No.8-------------------
+
+// var whether = prompt("Enter Number");
+// if( whether % 3 == 0 ){
+// document.write(whether + " " + "is divisible by 3")
+// }
+// else{
+//   document.write(whether + " " + "is not divisible by 3")
+// }
+
+// ------------- Question No.9-------------------
+
+var num = prompt("Enter Number");
+
+
+// ------------- Question No.9-------------------
