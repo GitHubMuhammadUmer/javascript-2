87e52c836bd8c28c78a10ace89276349e9775f31
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2023-10-21 12:09:13 -0700

    appcode

--- app.js
+++ app.js
@@ -269,6 +269,7 @@
 //   }
 
 // ------------- Question No.4-------------------
+
 // var fuel =prompt("Enter fue in car in litres");
 // if(fuel < 0.25){
 // document.write("Please refill the fuel in your car");
@@ -277,44 +278,81 @@
 // }
 
 // ------------- Question No.5-------------------
+
   //Run this script, & check whether alert message would be
              // displayed or not. Record the outputs.
-var a = 4;
-if (++a === 5){
-alert("given condition for variable a is true");
-}
- var b = 82;
-if (b++ === 83){
-alert("given condition for variable b is true");
-}
- var c = 12;
-if (c++ === 13){
-alert("condition 1 is true");
-}
-if (c === 13){
-alert("condition 2 is true");
-}
-if (++c < 14){
-alert("condition 3 is true");
-}
-if(c === 14){
-alert("condition 4 is true");
-}
-var materialCost = 20000;
-var laborCost = 2000;
-var totalCost = materialCost + laborCost;
-if (totalCost === laborCost + materialCost){
-alert("The cost equals");
-}
-if (true){
-alert("True");
-}
-if (false){
-alert("False");
-}
-if("car" < "cat"){
-  alert("car is smaller than cat");
-  }
+
+//a. 
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// output
+//  given condition for variable a is true
+
+
+//  b. 
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+// c.
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+//  output
+//   condition 2 is true
+
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// output
+//    condition 4 is true
+
+
+// d. 
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// output
+//    The cost equals
+
+
+// e.
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// output
+//    True
+
+
+
+// f.
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+//  output
+//     car is smaller than cat
+
+
+
 // ------------- Question No.6-------------------
 
 // var subjectMark1 = +prompt("Enter First Subject Marks");
